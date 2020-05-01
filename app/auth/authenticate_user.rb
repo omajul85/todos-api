57f3a4a58b545d8c7de42a1ebf8dd7e128de1be7
Omar Alvarez <omar.alvarez@dubitlimited.com>
2020-05-01 07:57:50 +0100

    Add AuthenticateUser service class with tests.

---
+++ app/auth/authenticate_user.rb
@@ -0,0 +1,24 @@
+# This class will be responsible for authenticating users via email and password.
+class AuthenticateUser
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  # Service entry point
+  def call
+    JsonWebToken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_reader :email, :password
+
+  def user
+    user = User.find_by_email(email)
+
+    return user if user && user.authenticate(password)
+
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+  end
+end
