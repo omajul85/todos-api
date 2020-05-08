aa9f1311ba4646d4a7e682c2e17f6139c33fd2ea
Omar Alvarez <omar.alvarez@dubitlimited.com>
2020-05-08 11:51:56 +0100

    Add API versioning and V1

---
+++ app/controllers/v1/todos_controller.rb
@@ -0,0 +1,52 @@
+module V1
+  class TodosController < ApplicationController
+    # GET /todos
+    def index
+      @todos = current_user.todos
+
+      json_response(@todos)
+    end
+
+    # POST /todos
+    # Note that we're using create! instead of create. The model will raise an exception
+    # ActiveRecord::RecordInvalid if is not valid. This way, we can avoid deep nested if statements in the controller.
+    # Thus, we rescue from this exception in the ExceptionHandler module.
+    def create
+      @todo = current_user.todos.create!(todo_params)
+
+      json_response(@todo, :created)
+    end
+
+    # GET /todos/:id
+    def show
+      @todo = current_user.todos.find(params[:id])
+
+      json_response(@todo)
+    end
+
+    # PUT /todos/:id
+    def update
+      @todo = current_user.todos.find(params[:id])
+
+      @todo.update(todo_params)
+
+      head :no_content
+    end
+
+    # DELETE /todos/:id
+    def destroy
+      @todo = current_user.todos.find(params[:id])
+
+      @todo.destroy
+
+      head :no_content
+    end
+
+    private
+
+    def todo_params
+      # whitelist params
+      params.permit(:title)
+    end
+  end
+end
